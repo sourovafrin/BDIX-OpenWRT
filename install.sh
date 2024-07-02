c59ee1494c9d0a4a9005e11612e6cb4b6f0d8e73
sourov <38147368+sourovafrin@users.noreply.github.com>
2024-07-02 22:38:32 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/emonbhuiyan/BDIX-OpenWRT/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/sourovafrin/BDIX-OpenWRT/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/sourovafrin/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix
 
 cd /
 clear
